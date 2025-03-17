22bd560b7c013e72a879b7c44ac733ec0d012a9a
wangwencaho <wenchaowang0@163.com>
2025-03-17 12:27:07 +0800

    update

---
+++ service/pages/activityDetail/activityDetail.js
@@ -0,0 +1,22 @@
+// activityDetail 页面
+Page({
+  data: {
+    webUrl: ''
+  },
+  onLoad(options) {
+    const url = decodeURIComponent(options.url);
+    console.log('Decoded URL:', url);
+    if (url.startsWith('')) {
+      this.setData({
+        webUrl: url
+      });
+    } else {
+      console.error('Invalid URL. web-view only supports HTTPS URLs.');
+      wx.showToast({
+        title: '无效的 URL，仅支持 HTTPS 链接',
+        icon: 'none'
+      });
+    }
+  }
+});
+
