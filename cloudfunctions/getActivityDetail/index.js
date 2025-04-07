40b2196d3ab562eff524db7a9b985a080e5ac080
Hu shan <hushan@kmoon.fun>
2025-04-07 14:52:35 +0800

    delete cloudfunctions.
    删除多余云函数

--- cloudfunctions/getActivityDetail/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-exports.main = async (event, context) => {
-    console.log("Hello World")
-    console.log(event)
-    console.log(event["non-exist"])
-    console.log(context)
-    return event
-};
